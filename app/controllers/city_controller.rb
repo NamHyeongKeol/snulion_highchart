2e155fc5b4f9e101a36402a5b0831e0b2587b8d0
NamHyeongKeol <grey_pastor@naver.com>
2017-05-08 02:19:19 +0900

    sorting.html.erb 만듦

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -5,5 +5,10 @@ class CityController < ApplicationController
 
   def sorting
     @cities = City.sorting_all(params[:sorting_params])
+
+    # app/views/sorting.html.erb가 없으면 없어도 되는 부분
+    respond_to do |format|
+      format.js
+    end
   end
 end
